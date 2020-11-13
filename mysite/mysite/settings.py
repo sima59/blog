6a6a2d6cca0803f2ab594cbd7c97b5c1208ced23
sima59 <simabageri@gmail.com>
2020-11-13 10:41:58 -0600

    add local host

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
       'ENGINE': 'django.db.backends.postgresql',
       'NAME': 'blog',
       'USER': 'blog',
-      'PASSWORD': '***********',
+      'PASSWORD': 'sima12345',
     }
 }
 
